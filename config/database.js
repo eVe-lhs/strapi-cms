011ea2a26541f92fc103f59a26d2072d467d8729
Lin Htet Swe <linhtetswe2012@gmail.com>
2021-08-20 08:00:13 +0630

    removed enviroment check

--- config/database.js
+++ config/database.js
@@ -1,37 +1,18 @@
-module.exports = ({ env }) => {
-  if (env("NODE_ENV") === "production") {
-    return {
-      defaultConnection: "default",
-      connections: {
-        default: {
-          connector: "mongoose",
-          settings: {
-            uri: env("DATABASE_URI"),
-            srv: env.bool("DATABASE_SRV", true),
-            port: env.int("DATABASE_PORT", 27017),
-            database: env("DATABASE_NAME"),
-          },
-          options: {
-            authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
-            ssl: env.bool("DATABASE_SSL", true),
-          },
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "mongoose",
+      settings: {
+        uri: env("DATABASE_URI"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME"),
       },
-    };
-  }
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          filename: env("DATABASE_FILENAME", ".tmp/data.db"),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+      options: {
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
-  };
-};
+  },
+});
